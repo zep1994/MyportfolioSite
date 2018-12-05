2d5de8c188b02d5ecdd1f437ab52cb0a24bc1d52
zep1994 <snaxbags@protonmail.com>
2018-12-04 18:07:46 -0600

    Initial Setup

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import LandingPage from './landingpage'
+
+const Main = () => { < Switch >
+    < Route exact path = "/"
+  component = {
+    LandingPage
+  } < /Switch>
+}
+
+export default Main;
