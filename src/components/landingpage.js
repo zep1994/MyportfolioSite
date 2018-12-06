15c4a459418dfb821d15fe1d4b9247225ef2dcea
zep1994 <snaxbags@protonmail.com>
2018-12-06 15:58:04 -0600

    Add Landing pages and tabs

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl'
 
 class Landing extends Component {
   render() {
     return (
-      <div><h1>Landing Page</h1></div>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://pbs.twimg.com/profile_images/857508248647282690/iZmsa9lG_400x400.jpg"
+              alt="avatar"
+              className="avatar-img"
+            />
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>HTML/CSS | Ruby on Rails | JavaScript | React | NodeJS | MongoDB | Bootstrap</p>
+              <div className="social-links">
+
+                <a href="https://github.com/zep1994" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github-square" aria-hidden="true" />
+                </a>
+
+                <a href="https://www.linkedin.com/in/twmatlock/" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                <a href="https://www.youtube.com/channel/UCAZV8spZCznfG4I79EQxb1g" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-youtube-square" aria-hidden="true" />
+                </a>
+
+                <a href="https://twitter.com/Snaxbags" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter-square" aria-hidden="true" />
+                </a>
+
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import { Tabs, Tab} from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeTab: 0
+    }
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return(
+        <div><h1>Ruby</h1></div>
+      )
+    } else if (this.state.activeTab === 1) {
+      return(
+        <div><h1>Sinatra</h1></div>
+      )
+    } else if (this.state.activeTab === 2) {
+      return(
+        <div><h1>Rails & JavaScript</h1></div>
+      )
+    } else if (this.state.activeTab === 3) {
+      return(
+        <div><h1>React</h1></div>
+      )
+    }
+  }
+
+
   render() {
     return (
-      <div><h1>Projects Page</h1></div>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>Ruby CLI</Tab>
+          <Tab>Sinatra</Tab>
+          <Tab>Rails & JavaScript</Tab>
+          <Tab>React</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          {this.toggleCategories()}
+        </section>
+      </div>
     )
   }
 }
