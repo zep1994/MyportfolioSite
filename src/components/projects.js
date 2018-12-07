58f5c41993cf491f6c14b83f2b18c610b678ed69
zep1994 <snaxbags@protonmail.com>
2018-12-06 19:11:34 -0600

    Add Project Cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab} from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
   constructor(props) {
@@ -12,7 +12,52 @@ class Projects extends Component {
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return(
-        <div><h1>Ruby</h1></div>
+      <div className="projects-grid">
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center/ cover'}}>React Project #1</CardTitle>
+          <CardText>
+            This is the description of the project
+          </CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>CodePen</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center/ cover'}}>React Project #1</CardTitle>
+          <CardText>
+            This is the description of the project
+          </CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>CodePen</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center/ cover'}}>React Project #1</CardTitle>
+          <CardText>
+            This is the description of the project
+          </CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>CodePen</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      </div>
       )
     } else if (this.state.activeTab === 1) {
       return(
@@ -40,9 +85,12 @@ class Projects extends Component {
           <Tab>React</Tab>
         </Tabs>
 
-        <section className="projects-grid">
-          {this.toggleCategories()}
-        </section>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
