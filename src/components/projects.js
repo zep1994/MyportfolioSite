a83bfa6e507d20aa67ce6bcc222bf0d03eb21951
zep1994 <snaxbags@protonmail.com>
2019-02-16 20:32:55 -0600

    Update Projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -54,6 +54,20 @@ class Projects extends Component {
             <IconButton name="share" />
           </CardMenu>
         </Card>
+
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*sZSVVtdP9TE3mUoGh4GoYA.png) center/ cover'}}>React Project #1</CardTitle>
+          <CardText>
+            This is the description of the project
+          </CardText>
+          <CardActions border>
+          <Button colored><a href="https://github.com/zep1994/Rails_Blog_with_Comments" target="_blank" rel="noopener noreferrer">Github</a></Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
       </div>
       )
     } else if (this.state.activeTab === 1) {
@@ -202,7 +216,7 @@ class Projects extends Component {
     return (
       <div className="category-tabs">
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>Ruby</Tab>
+          <Tab>Ruby & Ruby on Rails</Tab>
           <Tab>Sinatra</Tab>
           <Tab>Rails & JavaScript</Tab>
           <Tab>React</Tab>
